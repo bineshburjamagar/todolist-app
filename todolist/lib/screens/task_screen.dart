4ef0bfec8ed8202425b91345fd5e9ee2cab727e2
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-05-24 23:28:28 +0545

    ui designing

--- todolist/lib/screens/task_screen.dart
+++ todolist/lib/screens/task_screen.dart
@@ -5,6 +5,57 @@ class TasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color(0xFFEC6E0E),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color(0xFFEC6E0E),
+        child: const Icon(Icons.add_circle_outline),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.view_list_rounded,
+                    size: 30.0,
+                    color: Color(0xFFEC6E0E),
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(height: 10.0),
+                Text(
+                  'TO DO LIST',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  '666 Tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0))),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
