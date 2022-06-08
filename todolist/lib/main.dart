c5a1172db1924c1eb605f2aa29bd2f325431757c
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-06-08 21:18:24 +0545

    adding provider

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todolist/models/task_data.dart';
 import 'screens/task_screen.dart';
 
 void main() {
@@ -10,8 +12,11 @@ class ToDoList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: TasksScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => TaskData(),
+      child: MaterialApp(
+        home: TasksScreen(),
+      ),
     );
   }
 }
